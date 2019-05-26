2b73afde3d3f7b5a00670a7e9faf25d604365d03
digiboost <digiboostcrm@gmail.com>
2019-05-26 17:35:20 +0500

    expect vendor + uploads

---
+++ url-monitor/config.php
@@ -0,0 +1,11 @@
+<?php
+define('PSM_DB_HOST', '172.24.16.132');
+define('PSM_DB_PORT', '3306');
+define('PSM_DB_NAME', 'digiboost_monitoring');
+define('PSM_DB_USER', 'monitor');
+define('PSM_DB_PASS', '8eddB5EYTfywF979');
+define('PSM_DB_PREFIX', 'psm_');
+define('PSM_BASE_URL', 'https://digiboost.com/portal/url-monitor');
+
+define('PSM_CACHE_PATH', dirname(__FILE__).'/cache');
+define('PSM_CACHE_LIFETIME', 60);
---
+++ url-monitoring/config.php
@@ -0,0 +1,8 @@
+<?php
+define('PSM_DB_HOST', '172.24.16.132');
+define('PSM_DB_PORT', '3306');
+define('PSM_DB_NAME', 'digi_monitor');
+define('PSM_DB_USER', 'digi_monitor_user');
+define('PSM_DB_PASS', 'eKlta0xk98N4kS2w');
+define('PSM_DB_PREFIX', 'psm_');
+define('PSM_BASE_URL', 'http://digiboost.com/portal/url-monitoring');
